95e2fefa0449f021036f82fdde4eed3a87a42d88
taia <natalia.faraujo@b2wdigital.com>
2021-11-30 17:23:11 -0300

    ordernação GET topics/id

--- src/main/java/br/com/alura/forum/controller/TopicsController.java
+++ src/main/java/br/com/alura/forum/controller/TopicsController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -40,9 +41,9 @@ public class TopicsController {
 
     @GetMapping
     public Page<TopicDto> findList(@RequestParam(required = false) String nameCourse,
-                                   @RequestParam int page, @RequestParam int qtd) {
+                                   @RequestParam int page, @RequestParam int qtd, @RequestParam String sort) {
 
-        Pageable pageable = PageRequest.of(page, qtd);
+        Pageable pageable = PageRequest.of(page, qtd, Sort.Direction.ASC, sort);
 
         if (nameCourse == null) {
             Page<Topic> topics = topicRepository.findAll(pageable);
