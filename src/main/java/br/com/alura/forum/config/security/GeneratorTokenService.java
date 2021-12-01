fe9a2d9532750f89bebc9432318e146a5dc22752
taia <natalia.faraujo@b2wdigital.com>
2021-12-01 16:01:17 -0300

    gerando token com JWT

---
+++ src/main/java/br/com/alura/forum/config/security/GeneratorTokenService.java
@@ -0,0 +1,35 @@
+package br.com.alura.forum.config.security;
+
+import br.com.alura.forum.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class GeneratorTokenService {
+
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String generateToken(Authentication authentication) {
+
+        User logUser = (User) authentication.getPrincipal();
+        Date today = new Date();
+        Date expirationDate = new Date(today.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("Api do Fórum da Alura")
+                .setSubject(logUser.getId().toString())
+                .setIssuedAt(today)
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
---
+++ src/main/java/br/com/alura/forum/controller/AuthenticationController.java
@@ -0,0 +1,40 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.config.security.GeneratorTokenService;
+import br.com.alura.forum.controller.dto.TokenDto;
+import br.com.alura.forum.controller.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticManager;
+
+    @Autowired
+    private GeneratorTokenService generatorTokenService;
+
+    @PostMapping
+    public ResponseEntity<TokenDto> authenticate(@RequestBody @Valid LoginForm loginForm) {
+        UsernamePasswordAuthenticationToken loginData = loginForm.convert();
+
+        try {
+            Authentication authentication = authenticManager.authenticate(loginData);
+            String token = generatorTokenService.generateToken(authentication);
+            return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
